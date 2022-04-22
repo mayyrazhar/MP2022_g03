9923742b6b58a1c63a5cc8a68fc1924023f8fc74
ItsJustIzmeer <59471170+ItsJustIzmeer@users.noreply.github.com>
2022-04-22 23:39:31 +0800

    Bottom Navigation Bar

--- Login/app/src/main/java/com/example/speedy2/SecondActivity.java
+++ Login/app/src/main/java/com/example/speedy2/SecondActivity.java
@@ -2,6 +2,9 @@ package com.example.speedy2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,17 +13,44 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.speedy2.databinding.ActivitySecondBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SecondActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private Button logout;
+    ActivitySecondBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item ->{
+
+            switch (item.getItemId()){
+
+                case R.id.nav_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.nav_activity:
+                    replaceFragment(new ActivityFragment());
+                    break;
+                case R.id.nav_payment:
+                    replaceFragment(new PaymentFragment());
+                    break;
+                case R.id.nav_message:
+                    replaceFragment(new MessageFragment());
+                    break;
+                case R.id.nav_profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -61,4 +91,12 @@ public class SecondActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
