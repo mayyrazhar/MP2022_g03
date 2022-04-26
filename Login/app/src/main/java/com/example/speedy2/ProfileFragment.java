7514609801d04d88bd03a4022f66fe12c0c6f5f6
fdzliskndr <ahmdfadzil31@gmail.com>
2022-04-26 23:10:16 +0800

    LOGOUT ON PROFILE FRAGMENT
    CODEK
    1109 PM
    TUESDAY, 26 APR 2022

--- Login/app/src/main/java/com/example/speedy2/ProfileFragment.java
+++ Login/app/src/main/java/com/example/speedy2/ProfileFragment.java
@@ -1,19 +1,45 @@
 package com.example.speedy2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileFragment extends Fragment {
+
+    private FirebaseAuth firebaseAuth;
+    private Button logout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        Button logout = (Button) view.findViewById(R.id.btnLogout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+        return view;
     }
+
+    private void logout (){
+        firebaseAuth.signOut();
+        getActivity().finish();
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
+    }
+
 }
--- Login/app/src/main/java/com/example/speedy2/SecondActivity.java
+++ Login/app/src/main/java/com/example/speedy2/SecondActivity.java
@@ -54,42 +54,12 @@ public class SecondActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        logout = (Button) findViewById(R.id.btnLogout);
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Logout();
-            }
-        });
-    }
-
-    private void Logout (){
-        firebaseAuth.signOut();
-        finish();
-        startActivity(new Intent(SecondActivity.this, MainActivity.class));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId()){
-            case R.id.logoutMenu:{
-                Logout();
-                break;
-            }
-
-            case R.id.profileMenu:
-                startActivity(new Intent(SecondActivity.this, ProfileActivity.class));
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void replaceFragment(Fragment fragment){
