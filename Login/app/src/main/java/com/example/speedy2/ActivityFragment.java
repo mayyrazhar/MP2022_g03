85f95c4eddd9b8629920de53fe1dc16b273eaea3
fath1yaa <yayafathiya030602@gmail.com>
2022-04-28 23:40:08 +0800

    front-end second last attempt

--- Login/app/src/main/java/com/example/speedy2/ActivityFragment.java
+++ Login/app/src/main/java/com/example/speedy2/ActivityFragment.java
@@ -1,11 +1,13 @@
 package com.example.speedy2;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,8 +15,8 @@ import androidx.fragment.app.Fragment;
 
 public class ActivityFragment extends Fragment {
 
-    private Button carBookingHistory;
-    private Button scooterBookingHistory;
+    private ImageButton carBookingHistory;
+    private ImageButton scooterBookingHistory;
 
     @Nullable
     @Override
